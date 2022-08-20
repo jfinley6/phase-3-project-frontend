37845cefd77f60250f4e22ab3ba68bd2711fcb65
John Finley <johnfinleybremerton@gmail.com>
2022-08-19 20:29:39 -0600

    .

--- src/components/HomePagePicture.js
+++ src/components/HomePagePicture.js
@@ -2,8 +2,13 @@ import React from 'react'
 
 function HomePagePicture() {
   return (
-    <div className='homePagePicture'>
-      <img src="images/how-to-play-blackjack-lead.jpeg" alt="" style={{height: "75vh", width: "90vw"}}/>
+    <div className="homePagePicture">
+      <img
+        className="rounded-2xl"
+        src="images/how-to-play-blackjack-lead.jpeg"
+        alt=""
+        style={{ height: "75vh", width: "90vw" }}
+      />
     </div>
   );
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -8,7 +8,7 @@ function NavBar() {
         <button
           style={{ width: "100px" }}
           type="button"
-          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-3 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
         >
           Login
         </button>
