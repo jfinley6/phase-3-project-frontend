81b794261b2ee4b4137735be895f06a30448fc53
John Finley <johnfinleybremerton@gmail.com>
2022-08-19 19:52:20 -0600

    Added landing page with login and signup button

---
+++ src/components/HomePagePicture.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function HomePagePicture() {
+  return (
+    <div className='homePagePicture'>
+      <img src="images/how-to-play-blackjack-lead.jpeg" alt="" style={{height: "75vh", width: "90vw"}}/>
+    </div>
+  );
+}
+
+export default HomePagePicture
