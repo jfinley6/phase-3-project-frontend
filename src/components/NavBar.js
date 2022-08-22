20daca2d0e4460315186e575f21fb686b89bfcaa
John Finley <johnfinleybremerton@gmail.com>
2022-08-22 10:50:24 -0600

    Login signup button styling

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,26 +1,25 @@
 import React from "react";
+import Button from "react-bootstrap/Button";
 
 function NavBar({setShow, setShowSignup}) {
   return (
     <header className="flex items-center justify-between bg-gray-800 p-6 w-full">
       <h1 className="text-4xl text-gray-200">Blackjack App</h1>
       <div>
-        <button
-          style={{ width: "100px" }}
-          type="button"
+        <Button
+          style={{ width: "100px", marginRight: "10px" }}
           onClick={() => setShow(true)}
-          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-3 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+          variant="primary"
         >
           Login
-        </button>
-        <button
+        </Button>
+        <Button
           style={{ width: "100px" }}
-          type="button"
-          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5  dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
           onClick={() => setShowSignup(true)}
+          variant="primary"
         >
-          Sign Up
-        </button>
+          Signup
+        </Button>
       </div>
     </header>
   );
